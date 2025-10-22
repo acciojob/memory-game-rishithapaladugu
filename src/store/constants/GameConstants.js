7d4479879b7d727bf633e79484a9d953cf220e9c
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-22 10:33:08 +0530

    Delete src/store/constants/GameConstants.js

--- src/store/constants/GameConstants.js
+++
@@ -1,6 +0,0 @@
-export const START = "START";
-export const PAUSE = "PAUSE";
-export const QUIT = "QUIT";
-export const SETGAMEMODE = "SETGAMEMODE";
-export const SOLVEDPAIR = "SOLVEDPAIR";
-export const ADDFAILEDTRY = "ADDFAILEDTRY";
