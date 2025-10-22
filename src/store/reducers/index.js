9493c40cf2e05308d89698039d324a2060f899d0
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-22 10:33:55 +0530

    Delete src/store/reducers/index.js

--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import gameReducer from "./gameReducer";
-import cardReducer from "./cardReducer";
-
-export default combineReducers({
-  gameReducer,
-  cardReducer
-});
