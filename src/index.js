8c43b8e7ac03c0f1298534ed80776ee20c317c5f
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-22 10:43:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
+// src/index.js
 import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./store/reducers/index";
-import MemoryGameContainer from "./container/MemoryGameContainer/MemoryGameContainer";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // या App.css
 
-const store = createStore(rootReducer);
-
-function App() {
-  return (
-    <Provider store={store}>
-      <MemoryGameContainer />
-    </Provider>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
